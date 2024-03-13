0af685f21096da7d6520d272ab912bf4a058bfe6
Rob Godfrey <robcgodfrey@gmail.com>
2024-03-13 11:46:43 +0000

    completed set of functions to handle storage of JSON files requested from AV API and timestamp-based request gating

--- request_data_av_with_mongodb.py
+++ request_data_av_with_mongodb.py
@@ -3,7 +3,6 @@ from credentials import *
 
 from datetime import datetime, timedelta
 import json
-import os
 import pymongo
 
 try:
@@ -20,18 +19,38 @@ collection = db["timeseries_data_files"]
 my_api_key = alpha_vantage_api_key
 
 
-def main(maximum_stock_quote_age=86400):
-    # Read the last update timestamp from the "last_update.txt" file
+def main(mongodb_collection=collection, maximum_stock_quote_age=86400):
+    # If the time elapsed is greater than the maximum allowed age
+    if check_time_point(mongodb_collection=mongodb_collection, maximum_stock_quote_age=86400):
+        # check_time_point handles timestamp validation (and updating) with mongoDB database
+        print("A new request is being made.")
+        for ticker in ['BOL.PA', 'ODET.PA', 'UMG.AMS', 'VIV.PA']:
+            save_data_for(ticker=ticker)
+            print(f"updated data for {ticker}")
+
+    
+def save_data_for(ticker="TSCO.LON", my_api_key=my_api_key, mongodb_collection=collection):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={my_api_key}'
+    r = requests.get(url)
+    json_data = r.json()
+
+    file_name = f'{ticker.replace(".", "_")}.json'
+    
+    mongodb_collection.update_one({'_id': file_name},
+                          {'$set': {'data': json_data}},
+                          upsert=True)        
+
+
+def check_time_point(mongodb_collection=collection, maximum_stock_quote_age=86400):
+    # Read the last update timestamp from mongoDB
     try:
-        with open("data/last_update.txt", "r") as file:
-            last_update_str = file.read().strip()
-            if last_update_str:
-                last_update = datetime.strptime(last_update_str, "%Y-%m-%d %H:%M:%S")
-            else:
-                raise ValueError("Last update timestamp is empty")
-    except (FileNotFoundError, ValueError) as e:
-        # Handle the case when the file does not exist or the timestamp is invalid
+        last_update_str = mongodb_collection.find_one({'_id': 'last_update'})['last_update']        
+        last_update = datetime.strptime(last_update_str, "%Y-%m-%d %H:%M:%S")
+
+    except (FileNotFoundError, ValueError, TypeError) as e:
+        # Handle the case when the value does not exist or the timestamp is invalid
         print(f"Error reading last update timestamp: {e}")
+        print(f"Treat missing last update timestamp as 0AD (minimum)")
         last_update = datetime.min
 
     # Calculate the time elapsed since the last update
@@ -40,47 +59,25 @@ def main(maximum_stock_quote_age=86400):
 
     # If the time elapsed is greater than the maximum allowed age
     if time_elapsed > maximum_stock_quote_age:
-        print("A new request is being made.")
-        for ticker in ['BOL.PA', 'ODET.PA', 'UMG.AMS', 'VIV.PA']:
-            save_data_for(ticker=ticker)        
-
-        # Update the last update timestamp to the current datetime
         new_last_update = datetime.now()
-        
-        # Write the updated timestamp to the "last_update.txt" file
-        with open("data/last_update.txt", "w") as file:
-            file.write(new_last_update.strftime("%Y-%m-%d %H:%M:%S"))
+        new_last_update_str = new_last_update.strftime("%Y-%m-%d %H:%M:%S")
+
+        mongodb_collection.update_one({'_id': "last_update"},
+                                {'$set': {'last_update': new_last_update_str}},
+                                upsert=True)       
+              
+        return True
     else:
         print("Data up to date")
         print("Next request in ",maximum_stock_quote_age - time_elapsed,"s")
-
+        return False   
     
-def save_data_for(ticker="TSCO.LON", my_api_key=my_api_key):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={my_api_key}'
-    r = requests.get(url)
-    json_data = r.json()
-
-    current_directory = os.getcwd()
-    file_name = f'{ticker.replace(".", "_")}.json'
     
-    collection.update_one({'_id': file_name},
-                          {'$set': {'data': json_data}},
-                          upsert=True)        
-      
-    
-    file_path = os.path.join(current_directory, "data", file_name )
-    
-    print(file_path)
-    # file_path = os.path.join(file_path, 'downloads')
-    # file_path = os.path.join(file_path, f'{ticker.replace(".", "_")}.json')
-    
-    with open(file_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=2)
-
 
 if __name__ == "__main__":
     if (False):
         main()
     else:
+        #For testing functions while (WIP)
         save_data_for()
         print("test complete")
